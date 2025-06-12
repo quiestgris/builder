51f31efef4e5abfd4a58ab757e9f154783f6a0ec
quiestgris <sbaev72300@gmail.com>
2025-06-12 16:17:20 +0200

    V2.0

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,15 @@
+let leftItem = document.querySelector(".carousel-item.left-item");
+let rightItem = document.querySelector(".carousel-item.right-item");
+let middleItem = document.querySelector(".carousel-item.active");
+let positionOfActiveItem = middleItem.
+
+document.querySelector(".previous-page-btn").addEventListener("click", function() {
+    requestAnimationFrame(function () {
+        
+        leftItem.childNodes[1].childNodes[3].style.opacity = "0";
+        leftItem.style.zIndex = "2";
+        // leftItem.innerHTML = middleItem.innerHTML;
+        // middleItem.innerHTML = leftItemInnerHTML;
+    })
+})
+let nextPageBtn = document.querySelector(".next-page-btn");
